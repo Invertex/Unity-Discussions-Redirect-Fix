d5762945bd607219b827e35f17403d3d4a5f09d9
Dabbles in too many things <mitch.zais@gmail.com>
2024-07-18 16:09:20 -0700

    Update unity_discussions_redirect_fix.user.js

--- unity_discussions_redirect_fix.user.js
+++ unity_discussions_redirect_fix.user.js
@@ -4,8 +4,8 @@
 // @version      2024-07-18
 // @description  Fixes the redirect from old Discussions forum links to the new Discussions forum
 // @author       Invertex
-// @updateURL    https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/master/unity_discussions_redirect_fix.user.js
-// @downloadURL  https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/master/unity_discussions_redirect_fix.user.js
+// @updateURL    https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/main/unity_discussions_redirect_fix.user.js
+// @downloadURL  https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/main/unity_discussions_redirect_fix.user.js
 // @match        https://discussions.unity.com/threads/*.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=unity.com
 // @grant        none
