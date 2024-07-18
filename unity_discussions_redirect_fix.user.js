ae6d202ac8de14b6b76546557f11ad6a02c78aaa
Dabbles in too many things <mitch.zais@gmail.com>
2024-07-18 16:03:43 -0700

    Create unity_discussions_redirect_fix.user.js

---
+++ unity_discussions_redirect_fix.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Unity Discussions Redirect Fix
+// @namespace    Invertex
+// @version      2024-07-18
+// @description  Fixes the redirect from old Discussions forum links to the new Discussions forum
+// @author       Invertex
+// @updateURL    https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/master/unity_discussions_redirect_fix.user.js
+// @downloadURL  https://github.com/Invertex/Unity-Discussions-Redirect-Fix/raw/master/unity_discussions_redirect_fix.user.js
+// @match        https://discussions.unity.com/threads/*.*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=unity.com
+// @grant        none
+// @run-at document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let url = window.location.href;
+    let urlSplit = url.split('.com/threads/');
+    if(urlSplit.length > 1)
+    {
+        window.location.href = urlSplit[0] + '.com/t/' + urlSplit[1].replace('.','/');
+    }
+})();
